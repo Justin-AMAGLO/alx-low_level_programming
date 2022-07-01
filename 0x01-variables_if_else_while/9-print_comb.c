ec86062746085ae7873ce368d8aa4d0684b0ce49
Justin AMAGLO <amajust33@gmail.com>
2022-07-01 16:48:38 -0700

    task 1 to 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int jj = 48;
+
+	while (j < 58)
+	{
+		putchar(d);
+		if (j != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		j++;
+	}
+	putchar('\n');
+
+	return (0);
+}
